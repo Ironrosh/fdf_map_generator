33a79a1419bd82f49da9e172c60c194bbd786ac5
Ironrosh <rusyanchik@gmail.com>
2020-11-19 09:08:02 +0300

    Updated

--- generate_fdf_map.py
+++ generate_fdf_map.py
@@ -2,25 +2,26 @@ import sys
 from PIL import Image
 import numpy as np
 
-if len(sys.argv) != 2:
-	sys.exit("Usage: python3 generate_fdf_map.py path_to_file")
-picture = Image.open(sys.argv[1])
-picture = picture.convert('RGB')
-width, height = picture.size
-print("Your picture resolution is", width, "x", height)
-if width > height:
-	d = width / 10
-else:
-	d = height / 10
-pixels = picture.load()
-arr = [[0 for w in range(height)] for h in range(width)]
-for i in range(picture.size[0]):
-	for j in range(picture.size[1]):
-		summ = pixels[i,j][0] + pixels[i,j][1] + pixels[i,j][2]
-		arr[i][j] = d - (summ % d)
-fdf = open("out.fdf", 'w+')
-for i in range(picture.size[0]):
-	for j in range(picture.size[1]):
-		fdf.write("%d " % arr[i][j])
-	fdf.write("\n")
-fdf.close()
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		sys.exit("Usage: python3 generate_fdf_map.py path_to_file")
+	picture = Image.open(sys.argv[1])
+	picture = picture.convert('RGB')
+	width, height = picture.size
+	print("Your picture resolution is", width, "x", height)
+	if width > height:
+		d = width / 10
+	else:
+		d = height / 10
+	pixels = picture.load()
+	arr = [[0 for w in range(height)] for h in range(width)]
+	for i in range(picture.size[0]):
+		for j in range(picture.size[1]):
+			summ = pixels[i,j][0] + pixels[i,j][1] + pixels[i,j][2]
+			arr[i][j] = d - (summ % d)
+	fdf = open("out.fdf", 'w+')
+	for i in range(picture.size[0]):
+		for j in range(picture.size[1]):
+			fdf.write("%d " % arr[i][j])
+		fdf.write("\n")
+	fdf.close()
